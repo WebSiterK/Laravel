e6c1e627dd689ae4f7735941a10f4f667f07c5ec
WebSiterK <134371675+WebSiterK@users.noreply.github.com>
2024-06-28 18:46:57 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/planets', function () {
+    return ["Uranus", "Jupiter", "Mars", "Earth", "Saturn", "Pluto", "Neptune", "Venus"];
+});
