0bfa35a79c8852d49de293a315c1d604b9855a64
WebSiterK <134371675+WebSiterK@users.noreply.github.com>
2024-06-28 18:56:24 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,51 @@ Route::get('/planets', function () {
 
     return view('planets', compact('planets', 'filteredPlanet'));
 });
+
+Route::get('/planets/{planet}', function ($planet) {
+    $planets = [
+        'mars' => [
+            'name' => 'Mars',
+            'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
+        ],
+        'venus' => [
+            'name' => 'Venus',
+            'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
+        ],
+        'earth' => [
+            'name' => 'Earth',
+            'description' => 'Our home planet is the third planet from the Sun, and the only place we know of so far that\'s inhabited by living things.'
+        ],
+        'jupiter' => [
+            'name' => 'Jupiter',
+            'description' => 'Jupiter is a gas giant and doesn\'t have a solid surface, but it may have a solid inner core about the size of Earth.'
+        ],
+    ];
+
+    $selectedPlanet = $planets[$planet] ?? null;
+
+    return view('planet-details', compact('selectedPlanet'));
+});
+
+Route::get('/planets', function () {
+    $planets = [
+        [
+            'name' => 'Mars',
+            'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
+        ],
+        [
+            'name' => 'Venus',
+            'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
+        ],
+        [
+            'name' => 'Earth',
+            'description' => 'Our home planet is the third planet from the Sun, and the only place we know of so far that\'s inhabited by living things.'
+        ],
+        [
+            'name' => 'Jupiter',
+            'description' => 'Jupiter is a gas giant and doesn\'t have a solid surface, but it may have a solid inner core about the size of Earth.'
+        ],
+    ];
+
+    return view('planets-overview', compact('planets'));
+});
