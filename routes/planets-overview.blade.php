42866b7e220762d6c9cdb4d34554c258fa00e0b5
WebSiterK <134371675+WebSiterK@users.noreply.github.com>
2024-06-28 18:58:24 +0200

    Create planets-overview.blade.php

---
+++ routes/planets-overview.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Planets Overview</title>
+</head>
+<body>
+    <h1>List of Planets</h1>
+    <ul>
+        @foreach ($planets as $planet)
+            <li>
+                <a href="/planets/{{ strtolower($planet['name']) }}">{{ $planet['name'] }}</a>
+            </li>
+        @endforeach
+    </ul>
+</body>
+</html>
